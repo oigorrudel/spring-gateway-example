f89a0d5ecc3fd2cf502a87c351d0bce7dff569ff
Igor Rudel <igor.rudel@realizecfi.com.br>
2024-09-17 08:56:41 -0300

    --

--- api-gateway/src/main/java/br/xksoberbado/apigateway/config/SwaggerConfig.java
+++ api-gateway/src/main/java/br/xksoberbado/apigateway/config/SwaggerConfig.java
@@ -1,24 +1,16 @@
 package br.xksoberbado.apigateway.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.properties.SwaggerUiConfigParameters;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.cloud.gateway.server.mvc.config.GatewayMvcProperties;
-import org.springframework.cloud.gateway.server.mvc.config.RouteProperties;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class SwaggerConfig {
 
-    private final GatewayMvcProperties gatewayMvcProperties;
-
     @Bean
-    public CommandLineRunner openApiGroups(final SwaggerUiConfigParameters swaggerUiParameters) {
-        return args -> gatewayMvcProperties.getRoutes()
-            .stream()
-            .map(RouteProperties::getId)
-            .forEach(swaggerUiParameters::addGroup);
+    OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info().title("Api Gateway").version("1"));
     }
 }
