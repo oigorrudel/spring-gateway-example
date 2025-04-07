8bf95e48bd6b8aee16e45191072a34e0887c337b
Igor Rudel <igor.rudel@realizecfi.com.br>
2025-04-07 17:53:51 -0300

    --

--- api-x/src/main/java/br/xksoberbado/apix/web/controller/PersonController.java
+++ api-x/src/main/java/br/xksoberbado/apix/web/controller/PersonController.java
@@ -1,15 +1,12 @@
 package br.xksoberbado.apix.web.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Slf4j
 @CrossOrigin("*")
@@ -17,22 +14,33 @@ import java.util.UUID;
 @RequestMapping("v1/persons")
 public class PersonController {
 
-    private static final List<Person> PERSONS = List.of(
-        Person.of(UUID.randomUUID(), "Joao"),
-        Person.of(UUID.randomUUID(), "Maria")
+    private static List<Person> persons = List.of(
+        new Person(UUID.randomUUID(), "Joao"),
+        new Person(UUID.randomUUID(), "Maria")
     );
 
     @GetMapping
     public List<Person> get() {
         log.info("Getting persons.");
 
-        return PERSONS;
+        return persons;
     }
 
-    @Data
-    @AllArgsConstructor(staticName = "of")
-    private static class Person {
-        private UUID id;
-        private String name;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<Person> add(@RequestHeader(required = false) final String oheader,
+                            @RequestBody final Person person) {
+        log.info("Adding person.");
+
+        persons = Stream.concat(
+            persons.stream(),
+            Stream.of(person)
+        ).toList();
+
+        return persons;
+    }
+
+    private record Person(UUID id,
+                          String name) {
     }
 }
